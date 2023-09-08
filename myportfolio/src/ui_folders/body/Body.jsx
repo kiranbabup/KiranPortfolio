aeef9b3c8ac7c9a4a93749c1a0fdf252bc84a015
kiranbabup <kiran12146@gmail.com>
2023-09-08 23:35:13 +0530

    Made 2nd phase design

--- myportfolio/src/ui_folders/body/Body.jsx
+++ myportfolio/src/ui_folders/body/Body.jsx
@@ -1,5 +1,8 @@
 import './Body.css';
 import { assets } from '../../assets/assets';
+import htmlCssImage from "../../assets/html-css.png";
+import localStorageImage from "../../assets/localstorage.png"
+import mernImage from "../../assets/mern.png";
 function Body() {
   return (
     <div className="body">
@@ -14,7 +17,7 @@ function Body() {
             return (
               <aside>
                 <article className='imageBox'>
-                  <img src={a.png} alt='png' />
+                  <img src={a.png} alt='png' className='skillImages' />
                 </article>
               </aside>
             )
@@ -22,7 +25,11 @@ function Body() {
         }
       </article>
       <h2>My Projects</h2>
-
+      <article className='projectDetails'>
+        <div className='projectInfo'><button> <img src={htmlCssImage} alt="png" /> </button> <h5>Click the Logo to view the Projects which are built using HTML & CSS</h5></div>
+        <div className='projectInfo'><button> <img src={localStorageImage} alt="png" /> </button><h5>Click the Logo to view the Projects which are built using HTML, CSS, Zustand & LocalStorage</h5></div>
+        <div className='projectInfo'><button> <img src={mernImage} alt="png" /> </button><h5>Click the Logo to view the Projects which are built using MongoDB, Express, ReactJS & NodeJS</h5></div>
+      </article>
     </div>
   );
 }
--- myportfolio/src/ui_folders/header/Header.jsx
+++ myportfolio/src/ui_folders/header/Header.jsx
@@ -4,6 +4,7 @@ function Header() {
   return (
     <div className="header">
       <h1>PANIGRAHI KIRAN BABU</h1>
+      {/* <button>Contact Me</button> */}
     </div>
   );
 }
